d2cac5366c8d22fe1ce39167d901be9f4fc05bb1
Dmitry Ledentsov <dmlled@yahoo.com>
2024-02-11 21:07:46 +0100

    bench: send/receive extreme

--- test/bench_send_after.erl
+++ test/bench_send_after.erl
@@ -1,8 +1,10 @@
 -module(bench_send_after).
 
 -export([
+         bench_send_after_1ms/2,
          bench_send_after_10ms/2,
-         bench_send_after_100ms/2
+         bench_send_after_100ms/2,
+         bench_baseline_send_receive/2
         ]).
 
 send_after_ms_and_receive(Ms) ->
@@ -14,8 +16,18 @@ send_after_ms_and_receive(Ms) ->
             io:format("timeout out~n")
     end.
 
+bench_send_after_1ms(_, _) ->
+    send_after_ms_and_receive(1).
+
 bench_send_after_10ms(_, _) ->
     send_after_ms_and_receive(10).
 
 bench_send_after_100ms(_, _) ->
     send_after_ms_and_receive(100).
+
+bench_baseline_send_receive(_, _) ->
+    self() ! tick,
+    receive
+        tick -> 
+            ok
+    end.
