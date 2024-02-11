27636832f9097015e03b913bddc19e9e819e03be
Dmitry Ledentsov <dmlled@yahoo.com>
2024-02-11 21:05:30 +0100

    bench monotonic_time

--- test/bench_time.erl
+++ test/bench_time.erl
@@ -1,8 +1,12 @@
 -module(bench_time).
 
 -export([
-         bench_nano_time/2
+         bench_nano_time/2,
+         bench_monotonic_time/2
         ]).
 
 bench_nano_time(_, _) ->
     os:system_time(nanosecond).
+
+bench_monotonic_time(_, _) ->
+    erlang:monotonic_time(nanosecond).
